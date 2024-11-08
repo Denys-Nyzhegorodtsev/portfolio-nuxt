bfc8ad97e072d02f921e7cee7a5a96ce4e1410dd
Den <denis.nigegorodcev@gmail.com>
2024-11-08 17:59:36 +0200

    stable version

---
+++ components/MobileMenu/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="$style.mobileMenuWrapper">
+    <div
+      :class="['flex-c', $style.menuButton, isOpen ? $style.open : null]"
+      @click="toggleMenu"
+    >
+      <span :class="$style.firstLine" />
+      <span :class="$style.secondLine" />
+      <span :class="$style.thirdLine" />
+    </div>
+
+    <div :class="[$style.menuContent, isOpen ? $style.show : null]">
+      <div :class="['content-wrapper']">
+        <div :class="$style.menuWrapper">
+          <Menu type="mobile" />
+        </div>
+
+        <div :class="['flex-r', $style.buttonsWrapper]">
+          <LangSwitcher />
+          <ThemeSwitcher />
+        </div>
+
+        <Button
+          text="Download CV"
+          type="download"
+          size="big"
+          :url="`${baseURL}/public/docs/CV-Denys-Nyzhehorodtsev.pdf`"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue';
+  const baseURL = import.meta.env.BASE_URL || '/_nuxt/';
+
+  const isOpen = ref(false);
+
+  const toggleMenu = () => {
+    isOpen.value = !isOpen.value;
+  };
+</script>
+
+<style module type="scss" src="./MobileMenu.module.scss"></style>
